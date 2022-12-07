1a3b60debaa199fa217221ab0917bd123727d7fc
Willian Reichert <wnreichert@inf.ufrgs.br>
2022-12-06 22:30:32 -0300

    Implementação do A* com heurística hamming

--- Laboratório 1/Laboratorio1.py
+++ Laboratório 1/Laboratorio1.py
@@ -6,6 +6,9 @@
 # Jean Smaniotto Argoud   - Cartao 00275602
 # Willian Nunes Reichert  - Cartao 00134090
 
+import heapq
+
+
 SIMBOLOBRANCO = '_'  # Constante para indicar o espaco em branco no tabuleiro do 8-puzzle.
 POSICAOINICIAL = '2_3541687'  # Apenas para teste, remover depois
 OBJETIVO = '12345678_'
@@ -29,17 +32,22 @@ TOTALPOSICOES = 9
 
 
 class Nodo:  # Classe para armazenar todas as informacoes de cada nodo da arvore.
-    def __init__(self, estado_, pai_, acao_, custo_):
+    def __init__(self, estado_, pai_, acao_, custo_, heuristica_=0):
         self.estado = estado_
         self.pai = pai_
         self.acao = acao_
         self.custo = custo_
+        self.heuristica = heuristica_
+
+    def __lt__(self, other):  # Necessario para ordenar a fila de prioridades pelo custo do nodo.
+        return (self.custo + self.heuristica) < (other.custo + other.heuristica)
+
 
 def valida_texto(estado):  # Verifica se o texto representando a posicao do puzzle e valido.
     if len(estado) != TOTALPOSICOES:  # Testa se o estado possui nove caracteres.
         return False
 
-    if (len(set(estado)) != len(estado)):  # Testa se existem elementos repetidos.
+    if len(set(estado)) != len(estado):  # Testa se existem elementos repetidos.
         return False
 
     for i in estado:  # Testa se todos os caracteres sao numeros ou espacos em branco.
@@ -47,6 +55,7 @@ def valida_texto(estado):  # Verifica se o texto representando a posicao do puzz
             return False
 
     return True
+
 
 def valida_posicao(estado):  # Verifica se a posicao de entrada do puzzle possui solucao viavel.
     conta_inversoes = 0
@@ -57,17 +66,18 @@ def valida_posicao(estado):  # Verifica se a posicao de entrada do puzzle possui
             if estado[i] != SIMBOLOBRANCO and estado[j] != SIMBOLOBRANCO and estado[i] > estado[j]:
                 conta_inversoes += 1
 
-    if (conta_inversoes % 2 == 0):  # Se o numero de inversoes e par, entao o tabuleiro possui solucao valida.
+    if conta_inversoes % 2 == 0:  # Se o numero de inversoes e par, entao o tabuleiro possui solucao valida.
         return True
+    else:  # Se o numero de inversoes e impar, entao o tabuleiro nao possui solucao valida.
+        return False
 
-    else: # Se o numero de inversoes e impar, entao o tabuleiro nao possui solucao valida.
-        return False  
-    
- def permuta_branco(estado, posicao_branco, posicao_numero): # Movimenta a posicao em branco, no tabuleiro.
+
+def movimenta_branco(estado, posicao_branco, posicao_numero):  # Movimenta a posicao em branco, no tabuleiro.
     lista_posicao = list(estado)
     lista_posicao[posicao_branco], lista_posicao[posicao_numero] = lista_posicao[posicao_numero], lista_posicao[posicao_branco]
     novo_estado = ''.join(lista_posicao)
     return novo_estado
+
 
 def sucessor(estado):  # Recebe um estado e retorna uma lista de tuplas (ação, estado atingido) para cada ação que pode ser realizada no estado recebido como parâmetro.
     movimentos = []  # Inicializa a lista contendo as tuplas que indicam os movimentos.
@@ -118,10 +128,10 @@ def sucessor(estado):  # Recebe um estado e retorna uma lista de tuplas (ação,
 
     return movimentos
 
+
 def expande(nodo):  # Realiza todas as movimentacoes possiveis, a partir de um estado no tabuleiro.
     acoes = sucessor(nodo.estado)  # Obtem todas as movimentacoes possiveis no tabuleiro.
     lista_nodos = []  # Inicializa a lista contendo os nodos a serem retornados.
-    novo_nodo = Nodo('', None, '', 0)
 
     for i in range(len(acoes)):
         novo_nodo = Nodo(acoes[i][1], nodo, acoes[i][0], nodo.custo + 1)
@@ -129,11 +139,15 @@ def expande(nodo):  # Realiza todas as movimentacoes possiveis, a partir de um e
 
     return lista_nodos
 
-def bfs(estado_inicial):  # Realiza a busca em largura, ate encontrar a posicao que corresponde a solucao do jogo.
-    if valida_texto(estado_inicial) == False:  # Verifica se o texto (string) representando a posicao do puzzle e valido.
-        return None
 
-    elif valida_posicao(estado_inicial) == False:  # Verifica se a posicao de entrada do puzzle possui solucao viavel.
+def heuristica_hamming(estado):
+    return sum([estado[i] != OBJETIVO[i] for i in range(len(estado))])
+
+
+def bfs(estado_inicial):  # Realiza a busca em largura, ate encontrar a posicao que corresponde a solucao do jogo.
+    if valida_texto(estado_inicial) is False:  # Verifica se o texto (string) representando a posicao do puzzle e valido.
+        return None
+    if valida_posicao(estado_inicial) is False:  # Verifica se a posicao de entrada do puzzle possui solucao viavel.
         return None
 
     explorados = {}  # Inicializa o dicionario, contendo os nos explorados. O estado do tabuleiro sera usado como identificador.
@@ -149,7 +163,7 @@ def bfs(estado_inicial):  # Realiza a busca em largura, ate encontrar a posicao 
         v = fronteira.pop(0)  # Remove o PRIMEIRO elemento adicionado na FILA (FIFO - first in, first out).
 
         if v.estado == OBJETIVO:  # Casos em que foi encontrada a solucao.
-            while v.pai != None:  # Iteracao para resgatar o caminho de acoes do estado inicial ate a solucao.
+            while v.pai is not None:  # Iteracao para resgatar o caminho de acoes do estado inicial ate a solucao.
                 movimentos.append(v.acao)
                 v = v.pai
             movimentos.reverse()
@@ -159,11 +173,11 @@ def bfs(estado_inicial):  # Realiza a busca em largura, ate encontrar a posicao 
             explorados[v.estado] = v
             fronteira += expande(v)  # Adiciona todos os vizinhos de v na fronteira.
 
-def dfs(estado_inicial):  # Realiza a busca em largura, ate encontrar a posicao que corresponde a solucao do jogo.
-    if valida_texto(estado_inicial) == False:  # Verifica se o texto (string) representando a posicao do puzzle e valido.
-        return None
 
-    elif valida_posicao(estado_inicial) == False:  # Verifica se a posicao de entrada do puzzle possui solucao viavel.
+def dfs(estado_inicial):  # Realiza a busca em largura, ate encontrar a posicao que corresponde a solucao do jogo.
+    if valida_texto(estado_inicial) is False:  # Verifica se o texto (string) representando a posicao do puzzle e valido.
+        return None
+    if valida_posicao(estado_inicial) is False:  # Verifica se a posicao de entrada do puzzle possui solucao viavel.
         return None
 
     explorados = {}  # Inicializa o dicionario, contendo os nos explorados. O estado do tabuleiro sera usado como identificador.
@@ -178,7 +192,7 @@ def dfs(estado_inicial):  # Realiza a busca em largura, ate encontrar a posicao 
         v = fronteira.pop()  # Remove o ULTIMO elemento adicionado na PILHA (LIFO - last in, first out).
 
         if v.estado == OBJETIVO:  # Casos em que foi encontrada a solucao.
-            while v.pai != None:  # Iteracao para resgatar o caminho de acoes do estado inicial ate a solucao.
+            while v.pai is not None:  # Iteracao para resgatar o caminho de acoes do estado inicial ate a solucao.
                 caminho.append(v.acao)
                 v = v.pai
             caminho.reverse()
@@ -188,50 +202,28 @@ def dfs(estado_inicial):  # Realiza a busca em largura, ate encontrar a posicao 
             explorados[v.estado] = v
             fronteira += expande(v)  # Adiciona todos os vizinhos de v na fronteira.
 
-def heuristica_hamming(nodo):
-    distancia_hamming = 0
-    if nodo.estado[PRIMEIRAPOSICAO] != 1:
-        distancia_hamming += 1
-    if nodo.estado[SEGUNDAPOSICAO] != 2:
-        distancia_hamming += 1
-    if nodo.estado[TERCEIRAPOSICAO] != 3:
-        distancia_hamming += 1
-    if nodo.estado[QUARTAPOSICAO] != 4:
-        distancia_hamming += 1
-    if nodo.estado[QUINTAPOSICAO] != 5:
-        distancia_hamming += 1
-    if nodo.estado[SEXTAPOSICAO] != 6:
-        distancia_hamming += 1
-    if nodo.estado[SETIMAPOSICAO] != 7:
-        distancia_hamming += 1
-    if nodo.estado[OITAVAPOSICAO] != 8:
-        distancia_hamming += 1
-    if nodo.estado[NONAPOSICAO] != 9:
-        distancia_hamming += 1
-    return distancia_hamming+nodo.custo
 
-def astar_hamming(estado_inicial):
-    if valida_texto(estado_inicial) == False:  # Verifica se o texto (string) representando a posicao do puzzle e valido.
+def astar_hamming(
+        estado_inicial):  # Realiza a busca com A* hamming ate encontrar a posicao que corresponde a solucao do jogo.
+    if valida_texto(estado_inicial) is False:  # Verifica se o texto (string) representando a posicao do puzzle e valido.
+        return None
+    if valida_posicao(estado_inicial) is False:  # Verifica se a posicao de entrada do puzzle possui solucao viavel.
         return None
 
-    elif valida_posicao(estado_inicial) == False:  # Verifica se a posicao de entrada do puzzle possui solucao viavel.
-        return None
-
-    explorados = {}  # Inicializa o dicionario, contendo os nos explorados. O estado do tabuleiro sera usado como identificador.
-    raiz = Nodo(estado_inicial, None, '', CUSTOINICIAL)  # Inicializa a raiz, com o estado inicial do tabuleiro.
-    fronteira = [raiz]  # Inicializacao da FILA que representa a fronteira.
+    explorados = {}  # Inicializa o dicionario contendo os nos explorados. O estado do tabuleiro sera usado como identificador.
+    raiz = Nodo(estado_inicial, None, '', CUSTOINICIAL, heuristica_hamming(estado_inicial))  # Inicializa a raiz com o estado inicial do tabuleiro.
+    fronteira = [raiz]  # Inicializacao da FILA DE PRIORIDADE que representa a fronteira.
     movimentos = []  # Inicializacao da lista contendo os movimentos do estado inicial ate a solucao.
 
-    while True:  # Iteracao principal para realizar a busca com A* e distancia de Hamming, ate encontrar a solucao.
+    while True:  # Iteracao principal para realizar a busca com A* hamming ate encontrar a solucao.
 
         if fronteira == []:
             return None
 
-        fronteira.sort(key=heuristica_hamming) # <<<<<<<<<<<<<<<<<<<<<<< INEFICIENTE, ACHAR OUTRA ESTRUTURA PARA FRONTEIRA
-        v = fronteira.pop(0)  # Remove o PRIMEIRO elemento adicionado na FILA (FIFO - first in, first out).
+        v = heapq.heappop(fronteira)  # Remove o elemento que possui o MENOR CUSTO TOTAL (custo + hamming(estado)) adicionado na FILA DE PRIORIDADES.
 
         if v.estado == OBJETIVO:  # Casos em que foi encontrada a solucao.
-            while v.pai != None:  # Iteracao para resgatar o caminho de acoes do estado inicial ate a solucao.
+            while v.pai is not None:  # Iteracao para resgatar o caminho de acoes do estado inicial ate a solucao.
                 movimentos.append(v.acao)
                 v = v.pai
             movimentos.reverse()
@@ -239,15 +231,21 @@ def astar_hamming(estado_inicial):
 
         if v.estado not in explorados:
             explorados[v.estado] = v
-            fronteira += expande(v)  # Adiciona todos os vizinhos de v na fronteira.
+            for nodo in expande(v):  # Adiciona todos os vizinhos de v na fronteira.
+                nodo.heuristica = heuristica_hamming(nodo.estado)
+                heapq.heappush(fronteira, nodo)
+
 
 def inicia_programa():
-    #print(bfs(POSICAOINICIAL))
-    #print(dfs(POSICAOINICIAL))
-    print(astar_hamming(POSICAOINICIAL))
-    #print(astar_manhattan(POSICAOINICIAL))
+    # print(sucessor(POSICAOINICIAL))
+    print(f'bfs: {len(bfs(POSICAOINICIAL))} movimentos')
+    print(f'dfs: {len(dfs(POSICAOINICIAL))} movimentos')
+    print(f'a* hamming: {len(astar_hamming(POSICAOINICIAL))} movimentos')
+    # print(f'a* manhattan: {astar_manhattan(POSICAOINICIAL)}')
+
 
 def main():
     inicia_programa()
+
 
 main()
