0d75346c35280e39519c5fbde83f4b34abd96df8
Andrei <102610731+AndreiKoenich@users.noreply.github.com>
2022-12-17 11:40:14 -0300

    Update Laboratorio1.py

--- Laboratório 1/Laboratorio1.py
+++ Laboratório 1/Laboratorio1.py
@@ -48,7 +48,9 @@ def valida_texto(estado):  # Verifica se o texto representando a posicao do puzz
         return False
 
     for i in estado:  # Testa se todos os caracteres sao numeros ou espacos em branco.
-        if not i.isdigit() and i != SIMBOLOBRANCO:
+        if i == '0':
+            return False
+        elif not i.isdigit() and i != SIMBOLOBRANCO:
             return False
 
     return True
