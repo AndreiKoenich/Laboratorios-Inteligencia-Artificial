de52fe4c9e0ad1fc6a33d897569d88f0b624c98b
Andrei <102610731+AndreiKoenich@users.noreply.github.com>
2023-01-16 12:30:40 -0300

    Create Laboratorio2.py

---
+++ Laboratório 2/Laboratorio2.py
@@ -0,0 +1,125 @@
+# UNIVERSIDADE FEDERAL DO RIO GRANDE DO SUL - Semestre 2022/02
+
+# Trabalho 2 - Poda alfa-beta ou MCTS em Othello/Reversi
+
+# Andrei Pochmann Koenich - Cartão 00308680
+# Jean Smaniotto Argoud   - Cartão 00275602
+# Willian Nunes Reichert  - Cartão 00134090
+
+import time
+
+POSICAOINICIAL = [
+'........',
+'........',
+'........',
+'...WB...',
+'...BW...',
+'........',
+'........',
+'........',
+]
+
+POSICAOVAZIA = '.'
+BRANCO = 'W'
+PRETO = 'B'
+DIMENSAOTABULEIRO = 8
+SEMJOGADAS = -1
+
+def testa_posicao(tabuleiro,x,y,jogador):
+
+    if jogador == PRETO: # Verifica quem é o adversário.
+        adversario = BRANCO
+    else:
+        adversario = PRETO
+
+    # Abaixo, serão testadas as oito possíveis direçõs, para verificar se existe ao menos um lance válido.
+
+    i = 2 # Variável auxiliar, para percorrer as posições do tabuleiro (matriz 8x8) incrementando os índices.
+    if x-1 >= 0 and tabuleiro[x-1][y] == adversario: # Busca uma posição válida para colocar a peça, pela ESQUERDA.
+        while x-i >= 0:
+            if tabuleiro[x-i][y] == POSICAOVAZIA:
+                return True # Retorna verdadeiro, indicando que achou um lance válido nessa direção.
+            elif tabuleiro[x-i][y] == jogador:
+                break # Encerra a busca nessa direção, pois não há lances válidos.
+            i = i+1
+
+    i = 2 # Reinicia o valor da variável auxiliar para o próximo teste, em outra direção.
+    if x+1 < DIMENSAOTABULEIRO and tabuleiro[x+1][y] == adversario: # Busca uma posição válida para colocar a peça, pela DIREITA.
+        while x+i < DIMENSAOTABULEIRO:
+            if tabuleiro[x+i][y] == POSICAOVAZIA:
+                return True # Retorna verdadeiro, indicando que achou um lance válido nessa direção.
+            elif tabuleiro[x+i][y] == jogador:
+                break # Encerra a busca nessa direção, pois não há lances válidos.
+            i = i+1
+
+    i = 2 # Reinicia o valor da variável auxiliar para o próximo teste, em outra direção.
+    if y-1 >= 0 and tabuleiro[x][y-1] == adversario: # Busca uma posição válida para colocar a peça, por CIMA.
+        while y-i >= 0:
+            if tabuleiro[x][y-i] == POSICAOVAZIA:
+                return True # Retorna verdadeiro, indicando que achou um lance válido nessa direção.
+            elif tabuleiro[x][y-i] == jogador:
+                break # Encerra a busca nessa direção, pois não há lances válidos.
+            i = i+1
+
+    i = 2 # Reinicia o valor da variável auxiliar para o próximo teste, em outra direção.
+    if y+1 < DIMENSAOTABULEIRO and tabuleiro[x][y+1] == adversario: # Busca uma posição válida para colocar a peça, por BAIXO.
+        while y+i < DIMENSAOTABULEIRO:
+            if tabuleiro[x][y+i] == POSICAOVAZIA:
+                return True # Retorna verdadeiro, indicando que achou um lance válido nessa direção.
+            elif tabuleiro[x][y+i] == jogador:
+                break # Encerra a busca nessa direção, pois não há lances válidos.
+            i = i+1
+
+    i = 2 # Reinicia o valor da variável auxiliar para o próximo teste, em outra direção.
+    if x-1 >= 0 and y-1 >= 0 and tabuleiro[x-1][y-1] == adversario: # Busca uma posição válida para colocar a peça, pela DIAGONAL SUPERIOR ESQUERDA.
+        while x-i >= 0 and y-i >= 0:
+            if tabuleiro[x-i][y-i] == POSICAOVAZIA:
+                return True # Retorna verdadeiro, indicando que achou um lance válido nessa direção.
+            elif tabuleiro[x-i][y-i] == jogador:
+                break # Encerra a busca nessa direção, pois não há lances válidos.
+            i = i+1
+
+    i = 2 # Reinicia o valor da variável auxiliar para o próximo teste, em outra direção.
+    if x+1 < DIMENSAOTABULEIRO and y-1 >= 0 and tabuleiro[x+1][y-1] == adversario: # Busca uma posição válida para colocar a peça, pela DIAGONAL SUPERIOR DIREITA.
+        while x+i < DIMENSAOTABULEIRO and y-i >= 0:
+            if tabuleiro[x+i][y-i] == POSICAOVAZIA:
+                return True # Retorna verdadeiro, indicando que achou um lance válido nessa direção.
+            elif tabuleiro[x+i][y-i] == jogador:
+                break # Encerra a busca nessa direção, pois não há lances válidos.
+            i = i+1
+
+    i = 2 # Reinicia o valor da variável auxiliar para o próximo teste, em outra direção.
+    if x-i >= 0 and y+1 < DIMENSAOTABULEIRO and tabuleiro[x-1][y+1] == adversario: # Busca uma posição válida para colocar a peça, pela DIAGONAL INFERIOR ESQUERDA.
+        while x-i >= 0 and y+i < DIMENSAOTABULEIRO:
+            if tabuleiro[x-i][y+i] == POSICAOVAZIA:
+                return True # Retorna verdadeiro, indicando que achou um lance válido nessa direção.
+            elif tabuleiro[x-i][y+i] == jogador:
+                break # Encerra a busca nessa direção, pois não há lances válidos.
+            i = i+1
+
+    i = 2 # Reinicia o valor da variável auxiliar para o próximo teste, em outra direção.
+    if x+1 < DIMENSAOTABULEIRO and y+1 < DIMENSAOTABULEIRO and tabuleiro[x+1][y+1] == adversario: # Busca uma posição válida para colocar a peça, pela DIAGONAL INFERIOR DIREITA.
+        while x+i < DIMENSAOTABULEIRO and y+i < DIMENSAOTABULEIRO:
+            if tabuleiro[x+i][y+i] == POSICAOVAZIA:
+                return True # Retorna verdadeiro, indicando que achou um lance válido nessa direção.
+            elif tabuleiro[x+i][y+i] == jogador:
+                break # Encerra a busca nessa direção, pois não há lances válidos.
+            i = i+1
+
+    return False # Retorna falso, indicando que não há nenhum lance válido para o jogador.
+
+def testa_valida(tabuleiro,jogador):
+
+    for x in range(DIMENSAOTABULEIRO):
+        for y in range(DIMENSAOTABULEIRO):
+            if tabuleiro[x][y] == jogador and testa_posicao(tabuleiro,x,y,jogador) == True:
+                return True
+
+    return False
+
+def main():
+    print(testa_valida(POSICAOINICIAL,BRANCO))
+
+start_time = time.time()
+main()
+print("\n\n---TEMPO DE EXECUÇÃO: %s segundos ---\n\n" % (time.time() - start_time))
