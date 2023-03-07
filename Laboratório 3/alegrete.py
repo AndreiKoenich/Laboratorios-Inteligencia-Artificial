44555af262141d7f089fb09cb4841d2a86cf7155
Willian Reichert <wnreichert@inf.ufrgs.br>
2023-03-07 19:45:48 -0300

    Adição das docstrings nos métodos da regressão linear

--- Laboratório 3/alegrete.py
+++ Laboratório 3/alegrete.py
@@ -14,6 +14,13 @@ import numpy as np
 
 
 def compute_mse(theta_0, theta_1, data):
+    """
+    Calcula o erro quadratico medio
+    :param theta_0: float - intercepto da reta
+    :param theta_1: float -inclinacao da reta
+    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
+    :return: float - o erro quadratico medio
+    """
     # Extrai as features (área do terreno) e as respostas (preço) do conjunto de dados
     x = data[:, 0]
     y = data[:, 1]
@@ -28,6 +35,14 @@ def compute_mse(theta_0, theta_1, data):
 
 
 def step_gradient(theta_0, theta_1, data, alpha):
+    """
+    Executa uma atualização por descida do gradiente  e retorna os valores atualizados de theta_0 e theta_1.
+    :param theta_0: float - intercepto da reta
+    :param theta_1: float -inclinacao da reta
+    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
+    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
+    :return: float,float - os novos valores de theta_0 e theta_1, respectivamente
+    """
     # Extrai as features (área do terreno) e as respostas (preço) do conjunto de dados
     x = data[:, 0]
     y = data[:, 1]
@@ -50,6 +65,20 @@ def step_gradient(theta_0, theta_1, data, alpha):
 
 
 def fit(data, theta_0, theta_1, alpha, num_iterations):
+    """
+    Para cada época/iteração, executa uma atualização por descida de
+    gradiente e registra os valores atualizados de theta_0 e theta_1.
+    Ao final, retorna duas listas, uma com os theta_0 e outra com os theta_1
+    obtidos ao longo da execução (o último valor das listas deve
+    corresponder à última época/iteração).
+
+    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
+    :param theta_0: float - intercepto da reta
+    :param theta_1: float -inclinacao da reta
+    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
+    :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
+    :return: list,list - uma lista com os theta_0 e outra com os theta_1 obtidos ao longo da execução
+    """
     # Cria as listas para armazenar os valores atualizados de theta_0 e theta_1 a cada época/iteração
     theta_0_history = [theta_0]
     theta_1_history = [theta_1]
@@ -98,5 +127,5 @@ if __name__ == "__main__":
 
     print(f'MSE mínimo: {min(mse_hist):.8}')
     print(f'MSE máximo: {max(mse_hist):.8}')
-    print(f'MSE médio: {(sum(mse_hist)/len(mse_hist)):.8}')
+    print(f'MSE médio: {(sum(mse_hist) / len(mse_hist)):.8}')
     print(f'\n--- TEMPO DE EXECUÇÃO: {(time.time() - start_time):.6} segundos ---\n')
