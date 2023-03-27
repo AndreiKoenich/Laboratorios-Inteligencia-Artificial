9efa5577c7dc338cc94a39d7b42bb0d8eaf1fdc0
Willian Reichert <wnreichert@inf.ufrgs.br>
2023-03-26 22:29:23 -0300

    Adição do arquivo com a descrição do grupo no trabalho 4

---
+++ Laboratório 4/group.md
@@ -0,0 +1,7 @@
+UNIVERSIDADE FEDERAL DO RIO GRANDE DO SUL - Semestre 2022/02
+
+Trabalho 4 - Aprendizado por Reforço
+
+Andrei Pochmann Koenich - Cartão 00308680 - Turma A
+Jean Smaniotto Argoud - Cartão 00275602 - Turma B
+Willian Nunes Reichert - Cartão 00134090 - Turma B
--- Laboratório 4/src/qlearningAgents.py
+++ Laboratório 4/src/qlearningAgents.py
@@ -11,6 +11,13 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
+# UNIVERSIDADE FEDERAL DO RIO GRANDE DO SUL - Semestre 2022/02
+#
+# Trabalho 4 - Aprendizado por Reforço
+#
+# Andrei Pochmann Koenich - Cartão 00308680 - Turma A
+# Jean Smaniotto Argoud - Cartão 00275602 - Turma B
+# Willian Nunes Reichert - Cartão 00134090 - Turma B
 
 from game import *
 from learningAgents import ReinforcementAgent
